f4503dbe228c637b17ae3949c22d4abae2d0fb26
EduardoHaibleForti <eduardo.hf@puccampinas.edu.br>
2024-08-26 11:10:28 -0300

    Add files via upload

--- AulaRobotica26824.c
+++ AulaRobotica26824.c
@@ -1,1 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int n,x;
+    printf("Digite um numero inteiro positivo: ");
+    scanf("%d",&n);
+
+    x=2;
+    while(x<n) {
+        if(n%x==0){
+            printf("%d nao eh primo",n);
+            break;
+        }
+        x++;
+
+    }
+
+    if(x=n){
+        printf("%d eh primo",n);
+
+    }
+    return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
